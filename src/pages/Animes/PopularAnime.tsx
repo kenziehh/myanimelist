c652c43ce394cc4b2bec2da2d49ca8a8dda452e8
kenziehh <ghaisani.nurani@gmail.com>
2024-01-16 14:38:49 +0700

    ganti laptop

---
+++ src/pages/Animes/PopularAnime.tsx
@@ -0,0 +1,16 @@
+import AnimeCard from "@components/AnimeCard"
+
+const PopularAnime = () => {
+  return (
+    <section className="container">
+      <AnimeCard/>
+      <AnimeCard/>
+      <AnimeCard/>
+      <AnimeCard/>
+
+    </section>
+  )
+}
+
+export default PopularAnime
+
---
+++ src/pages/Home/TopAnime.tsx
@@ -0,0 +1,18 @@
+import { useQuery } from "@tanstack/react-query"
+
+const TopAnime = () => {
+
+  const {}=useQuery({
+    queryFn:async()=>{
+      const[]
+    }
+  })
+  const animes=await fetch('')
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TopAnime
