075e50a9c34e563a27c8e0948f6576e62453c963
kenziehh <ghaisani.nurani@gmail.com>
2024-01-17 13:27:40 +0700

    add useClickOutside hook and update import alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from 'path';
+import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
-      "@components": path.resolve(__dirname,"./src/components"),
-      "@pages": path.resolve(__dirname,"./src/pages"),
-      "@services": path.resolve(__dirname,"./src/services"),
-      "@utils": path.resolve(__dirname,"./src/utils"),
-      "@models": path.resolve(__dirname,"./src/models"),
-
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@services": path.resolve(__dirname, "./src/services"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
+      "@models": path.resolve(__dirname, "./src/models"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
     },
   },
   plugins: [react()],
