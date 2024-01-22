c89ba4ca7552aedb9b7967f1e4b938c26dbb5764
kenziehh <ghaisani.nurani@gmail.com>
2024-01-22 23:10:51 +0700

    add truncate on card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,6 @@
-import { AnimeItem} from "@models/animeItem";
+import { AnimeItem } from "@models/animeItem";
 import { MangaItem } from "@models/mangaItem";
+import { truncateText } from "@utils/truncateText";
 
 type CardData = AnimeItem | MangaItem;
 interface CardProps {
@@ -16,12 +17,12 @@ const Card: React.FC<CardProps> = ({ data, onClick, className }) => {
   };
   return (
     <div
-      className={`flex gap-8 items-start flex-col shadow-xl rounded-xl border-black h-[500px] ${className}`}
+      className={`flex gap-8 items-start flex-col shadow-xl rounded-xl border-black h-[530px] ${className}`}
       onClick={handleClick}
     >
       <img className="w-[300px] h-[400px]" src={data.images.webp.image_url} />
-      <div className="flex flex-col pl-4">
-        <h6>{data.title}</h6>
+      <div className="flex flex-col gap-3 pl-4">
+        <h6>{truncateText(data.title, 50)}</h6>
         <p>{data.score}</p>
       </div>
     </div>
